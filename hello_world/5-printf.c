0328fd935726cd2fb7775e726dd173191da70c08
devark28 <e.shimwa1@alustudent.com>
2026-01-09 23:23:24 +0200

    fix indentation in 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - prints: with proper grammar, but ...
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
