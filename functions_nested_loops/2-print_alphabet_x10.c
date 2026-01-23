35f56a650037d34405f4470e420b9325fdbc64d1
devark28 <e.shimwa1@alustudent.com>
2026-01-23 19:29:07 +0200

    add function to print the alphabet ten times in 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints _putchar
+ * print_alphabet_x10 - prints _putchar
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	int j;
