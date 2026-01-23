3621909679d9148272608183e668648c6dc7527f
devark28 <e.shimwa1@alustudent.com>
2026-01-23 22:36:50 +0200

    add function to print all natural numbers up to 98 in 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,6 +12,7 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		int i;
+
 		for (i = n; i > 98; i--)
 			printf("%d, ", i);
 		printf("%d\n", i);
@@ -19,18 +20,9 @@ void print_to_98(int n)
 	else
 	{
 		int i;
+
 		for (i = n; i < 98; i++)
 			printf("%d, ", i);
 		printf("%d\n", i);
 	}
 }
-
-/*int main(void)
-{
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
-}*/
