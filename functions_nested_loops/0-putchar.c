e78c4fc6ce5d4ba1e506a4cf4c3010a800d0bbb2
devark28 <e.shimwa1@alustudent.com>
2026-01-23 19:16:12 +0200

    add program to print "_putchar" using puts in 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,12 +4,12 @@
  * main - prints _putchar
  * Return: 0
  */
-int main(void)
-{
+int main(void) {
 	const char str[9] = "_putchar\n";
 	int i;
-	for (i = 0; i < 9; i++) {
+
+	for (i = 0; i < 9; i++)
 		_putchar(str[i]);
-	}
+
 	return (0);
 }
