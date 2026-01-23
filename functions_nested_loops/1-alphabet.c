cdf875b877079556e396985bc136163dde3bf14c
devark28 <e.shimwa1@alustudent.com>
2026-01-23 19:26:14 +0200

    add function to print the alphabet in 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - prints _putchar
+ * print_alphabet - prints _putchar
  * Return: void
  */
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i < 123; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 
 	_putchar('\n');
