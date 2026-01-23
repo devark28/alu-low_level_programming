f4e8bf997b0cbb066a1e595ce244d4243c819f2e
devark28 <e.shimwa1@alustudent.com>
2026-01-23 21:00:11 +0200

    add function to check if a character is alphabet in 4-isalpha.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,14 @@
 #ifndef _main_h
 #define _main_h
 
+int _putchar(char c);
+
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
-int _putchar(char c);
-
 int _islower(int c);
+
+int _isalpha(int c);
 
 #endif
