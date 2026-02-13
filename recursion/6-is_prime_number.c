2d04eb19dab56b953ebe8f64f353c30a2a011027
devark28 <e.shimwa1@alustudent.com>
2026-02-13 18:57:06 +0200

    add function to check if a number is prime in 6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+/**
+ * check_prime - function that checks inputted number is prime or not
+ * @n: number
+ * @d: number divisor
+ * Return: either 1 or 0
+ */
+int check_prime(int n, int d)
+{
+	if (d * d > n)
+		return 1;
+
+	if (n % d == 0)
+		return 0;
+
+	return check_prime(n, d + 1);
+}
+
+/**
+ * is_prime_number - function checks if number is prime
+ * @n: number
+ * Return: either 0 or 1
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return 0;
+
+	return check_prime(n, 2);
+}
--- recursion/main.h
+++ recursion/main.h
@@ -13,4 +13,6 @@ int factorial(int n);
 
 int _pow_recursion(int x, int y);
 
+int is_prime_number(int n);
+
 #endif
