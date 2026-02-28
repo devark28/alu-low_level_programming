0608e8e8fd9393f078c719a3dfe20f22511d76c2
devark28 <e.shimwa1@alustudent.com>
2026-02-28 06:03:55 +0200

    add functions to manage dog structures in dog.h and related source files

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,7 +11,8 @@ typedef struct dog dog_t;
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * Description: struct called "dog" that stores its name, its age and the name of its owner.
+ * Description: struct called "dog" that stores its name,
+ * its age and the name of its owner.
  */
 struct dog
 {
