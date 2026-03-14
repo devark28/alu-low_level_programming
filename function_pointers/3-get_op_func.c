33a7cea2fe38eb2afdfef5ac3fdbd44bca7409fe
devark28 <e.shimwa1@alustudent.com>
2026-03-14 19:51:16 +0200

    add calculator functions and operation selection in 3-calc.h, 3-get_op_func.c, 3-main.c, and 3-op_functions.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,7 +10,14 @@ int (*get_op_func(char *s))(int a, int b)
 {
 	int i = 0;
 
-	op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
 	while (i < 5)
 	{
